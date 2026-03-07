de9ad8e56d180227c5e855d560ca6df7025dbc1a
Anshgupta2906 <anshgupta7785@gmail.com>
2026-03-07 12:02:01 +0530

    learnt about post method

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
+from fastapi.params import Body  
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Message": " hello my bestfriend!!!!!!"}
+    return {"Message": " hello my bestfriend!!!!!!!!!"}
 
 @app.get("/posts")
 def get_post():
     return{"data":"this is your post"}
+
+@app.post("/createpost")
+def create_post(payLoad:dict = Body(...)):
+    print(payLoad)
+    return{"new posts":f"title{payLoad['title']} content:{payLoad['content']}"}
+
+
+
