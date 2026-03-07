0a4ca968a4c0d13b76b8f7c2bcd4f7eb8c808377
Anshgupta2906 <anshgupta7785@gmail.com>
2026-03-07 16:00:13 +0530

    learnt about pydantic lib

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 from fastapi import FastAPI
 from fastapi.params import Body  
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
 
+class post(BaseModel):
+    title: str
+    content: str
+    published:bool= False
+
+my_posts=[{"title":"title of post 1", "content":"content of post 1","id":1},
+{"title":"favourite food","content":"i love momos","id":2}]
+
 @app.get("/")
 def read_root():
-    return {"Message": " hello my bestfriend!!!!!!!!!"}
+    return {"Message": " hello my bestfriend!!!!!!!!!!!"}
 
 @app.get("/posts")
 def get_post():
-    return{"data":"this is your post"}
+    return{"data":my_posts}
 
-@app.post("/createpost")
-def create_post(payLoad:dict = Body(...)):
-    print(payLoad)
-    return{"new posts":f"title{payLoad['title']} content:{payLoad['content']}"}
+@app.post("/posts")
+def create_posts(post:post):
+    post_dict=post.dict()
+    post_dict['id'] = randrange(0,1000000)
+    my_posts.append(post_dict)
+    return{"data":post_dict}
 
+#title str,content str, category,bool published or draft
 
 
