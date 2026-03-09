870984aba21361e0c58e53a5e9ccc1b4a68c0b10
Anshgupta2906 <anshgupta7785@gmail.com>
2026-03-09 20:15:54 +0530

    making this project to my long term project FITBHARATapi

--- main.py
+++ main.py
@@ -6,27 +6,28 @@ from random import randrange
 app = FastAPI()
 
 
-class post(BaseModel):
-    title: str
-    content: str
+class Client(BaseModel):
+    name: str
+    primary_goal: str
+    body_weight:int
     published:bool= False
 
-my_posts=[{"title":"title of post 1", "content":"content of post 1","id":1},
-{"title":"favourite food","content":"i love momos","id":2}]
+clients_db=[{"Name":"Name of the client", "Goal":"specific goal of client 1","id":1},
+{"Name":"Ankit kumar","Goal":"Body recompositon","id":2}]
 
 @app.get("/")
 def read_root():
-    return {"Message": " hello my bestfriend!!!!!!!!!!!"}
+    return {"Message": " How are you feeling"}
 
-@app.get("/posts")
-def get_post():
-    return{"data":my_posts}
+@app.get("/goals")
+def get_goals():
+    return{"goal":clients_db}
 
-@app.post("/posts")
-def create_posts(post:post):
-    post_dict=post.dict()
+@app.post("/clients")
+def body_weigth(BW:Client):
+    post_dict=BW.dict()
     post_dict['id'] = randrange(0,1000000)
-    my_posts.append(post_dict)
+    clients_db.append(post_dict)
     return{"data":post_dict}
 
 #title str,content str, category,bool published or draft
